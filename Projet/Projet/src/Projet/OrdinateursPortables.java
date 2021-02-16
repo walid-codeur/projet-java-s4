ad5ad2b505a11b3aa2db351fd10f2ee194bce1f8
walid-codeur <78789317+walid-codeur@users.noreply.github.com>
2021-02-16 09:38:58 +0100

    Update OrdinateursPortables.java

--- Projet/Projet/src/Projet/OrdinateursPortables.java
+++ Projet/Projet/src/Projet/OrdinateursPortables.java
@@ -7,7 +7,8 @@ public class OrdinateursPortables extends PostesClients {
 		super (ip, passerelle, masque);
 	}
 	
-	private String ip;
-	private String passerelle;
-	private String masque;
+	// a activer quand on declenche un bouton ou qq chose
+	public String toString() {
+		return "Cet ordinateur portable a pour ip : " + getip() + " avec le masque suivant : " + getmasque() + ", a pour passerelle : " + getpasserelle();
+	}
 }
