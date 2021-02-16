613d38dad50ba79df299a68fc980416fe77f8d29
walid-codeur <78789317+walid-codeur@users.noreply.github.com>
2021-02-16 10:47:05 +0100

    Update Test.java

--- Projet/Projet/src/Projet/Test.java
+++ Projet/Projet/src/Projet/Test.java
@@ -3,6 +3,18 @@ package Projet;
 public class Test {
   
   public static void main(String[] args){
-  
+    OrdinateursFixes of1, of2;
+    OrdinateursPortables op1, op2;
+    
+    of1 = new OrdinateursFixes ("192.168.1.1","192.168.1.254","/24");
+    of2 = new OrdinateursFixes ("192.168.1.2","192.168.1.254","/24");
+    op1 = new OrdinateursPortables ("192.168.1.3","192.168.1.254","/24");
+    op2 = new OrdinateursPortables ("192.168.1.4","192.168.1.254","/24");
+    
+    System.out.println(of1);
+    System.out.println(of2);
+    System.out.println(op1);
+    System.out.println(op2);
+    
   }
 }
