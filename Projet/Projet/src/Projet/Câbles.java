922cd72cddfa9eb70f10b3a0e16d47005d74fa67
Ograity <39776982+Ograity@users.noreply.github.com>
2021-03-15 12:09:40 +0100

    Update Câbles.java

--- Projet/Projet/src/Projet/Câbles.java
+++ Projet/Projet/src/Projet/Câbles.java
@@ -1,5 +1,12 @@
 package Projet;
 
 public class Câbles {
-  
+  public Cable1(int pc1, int switch){
+    pc1 = 1;
+    switch = 1;
+  }
+  public Cable2(int switch, int pc2){
+    switch = 1;
+    pc2 = 1;
+  }
 }
